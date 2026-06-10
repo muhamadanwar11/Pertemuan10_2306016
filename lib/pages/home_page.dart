90be6111ce98e4b82a7206d552c7eeff0ecabd97
Abizar <abizaralgifari515@gmail.com>
2026-06-10 16:31:38 +0700

    update pertemuan ke 11

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -152,87 +152,145 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Container(
-            height: 120,
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 249, 250, 250),
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withAlpha(25),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: const Offset(0, 3),
+          child: Column(
+            children: [
+              Container(
+                height: 120,
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 249, 250, 250),
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withAlpha(25),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 28,
-                  backgroundColor: Colors.teal,
-                  backgroundImage: const NetworkImage("https://picsum.photos/seed/picsum/450/300"),
-                ),
-                const SizedBox(width: 15),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "hii,selamat datang $username",
-                        style: TextStyle(fontSize: 14, color: Colors.grey[600]),
-                      ),
-                      const SizedBox(height: 4),
-                      Row(
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 28,
+                      backgroundColor: Colors.teal,
+                      backgroundImage: const NetworkImage("https://picsum.photos/seed/picsum/450/300"),
+                    ),
+                    const SizedBox(width: 15),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text(
-                            username,
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey[800],
-                            ),
+                            "hii,selamat datang $username",
+                            style: TextStyle(fontSize: 14, color: Colors.grey[600]),
                           ),
-                          const SizedBox(width: 6),
-                          const Icon(
-                            Icons.verified,
-                            color: Colors.blue,
-                            size: 18,
+                          const SizedBox(height: 4),
+                          Row(
+                            children: [
+                              Text(
+                                username,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey[800],
+                                ),
+                              ),
+                              const SizedBox(width: 6),
+                              const Icon(
+                                Icons.verified,
+                                color: Colors.blue,
+                                size: 18,
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                ),
-                Stack(
-                  children: [
-                    IconButton(
-                      onPressed: () async {
-                        await logout();
-                      },
-                      icon: const Icon(Icons.logout, color: Colors.red),
                     ),
-                    Positioned(
-                      right: 0,
-                      top: 0,
-                      child: Container(
-                        width: 8,
-                        height: 8,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          shape: BoxShape.circle,
-                          border: Border.all(color: Colors.white, width: 1),
+                    Stack(
+                      children: [
+                        IconButton(
+                          onPressed: () async {
+                            await logout();
+                          },
+                          icon: const Icon(Icons.logout, color: Colors.red),
                         ),
-                      ),
+                        Positioned(
+                          right: 0,
+                          top: 0,
+                          child: Container(
+                            width: 8,
+                            height: 8,
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              shape: BoxShape.circle,
+                              border: Border.all(color: Colors.white, width: 1),
+                            ),
+                          ),
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: products.isEmpty
+                    ? const Center(child: Text("Belum ada produk"))
+                    : ListView.builder(
+                        itemCount: products.length,
+                        itemBuilder: (context, index) {
+                          final product = products[index];
+                          return Card(
+                            margin: const EdgeInsets.only(bottom: 12),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ), 
+                            child: ListTile(
+                              contentPadding: const EdgeInsets.all(15),
+                              title: Text(
+                                product.name,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ), 
+                              ), 
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const SizedBox(height: 5),
+                                  Text("Rp ${product.price}"),
+                                  const SizedBox(height: 5),
+                                  Text(product.description),
+                                ],
+                              ), 
+                              leading: IconButton(
+                                icon: const Icon(
+                                  Icons.edit,
+                                  color: Colors.orange,
+                                ), 
+                                onPressed: () => showForm(product: products[index], index: index),
+                              ), 
+                              trailing: IconButton(
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
+                                ), 
+                                onPressed: () => deleteProduct(index),
+                              ), 
+                            ), 
+                          );
+                        },
+                      ),
+              ),
+            ],
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showForm(),
+        backgroundColor: Colors.teal,
+        child: const Icon(Icons.add, color: Colors.white),
       ),
     );
   }
